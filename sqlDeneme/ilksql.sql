9f57a1262c7bfcbb38f4bc21888987df27eddfb6
Ahmet Karaboğa <130494000+akaraboga@users.noreply.github.com>
2024-11-13 17:53:42 +0300

    Add files via upload

---
+++ sqlDeneme/ilksql.sql
@@ -0,0 +1,16 @@
+--SELECT title,description from film
+--SELECT * FROM film
+--SELECT * FROM film WHERE replacement_Cost = 14.99;
+--Select * from film WHERE  title ='Alone Trip';
+
+--Select *from film Where rental_rate  <= 90
+
+Select * From actor 
+where  first_name='Penelope' or first_name='Bob' ; 
+
+Select * From film
+where not rental_rate = 2.99;	
+
+Select * From film
+where not rental_rate = 4.99;	
+
